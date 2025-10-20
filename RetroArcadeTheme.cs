3f528c74071a03d0ea43b184f0e43e3f2f0a6dd3
Chris <morenochristopher580@gmail.com>
2025-10-19 19:53:09 -0500

    Update RetroArcadeTheme.cs

--- RetroArcadeTheme.cs
+++ RetroArcadeTheme.cs
@@ -12,15 +12,18 @@ namespace NekoBeats
         {
             if (frequencies == null) return;
 
-            double barWidth = 400.0 / frequencies.Length;
-            double maxHeight = 150;
-            var random = new Random();
+            double screenWidth = 1920;
+            double screenHeight = 1080;
+            double barWidth = screenWidth / frequencies.Length;
+            double maxHeight = screenHeight * 0.9;
 
             for (int i = 0; i < frequencies.Length; i++)
             {
-                double height = (frequencies[i] / 100.0) * maxHeight;
+                // Boost sensitivity
+                double boostedValue = frequencies[i] * 1.8;
+                double height = (boostedValue / 100.0) * maxHeight;
                 double x = i * barWidth;
-                double y = 200 - height;
+                double y = screenHeight - height;
 
                 // 8-bit style colors
                 var colors = new[]
@@ -30,23 +33,27 @@ namespace NekoBeats
                 };
                 var color = colors[i % colors.Length];
 
-                // Pixelated look
+                // Pixelated look with larger blocks for fullscreen
                 var brush = new SolidColorBrush(color);
                 var pen = new Pen(Brushes.Black, 1);
 
-                // Draw pixel blocks
-                for (double blockY = y; blockY < 200; blockY += 4)
+                // Draw larger pixel blocks for fullscreen
+                for (double blockY = y; blockY < screenHeight; blockY += 8)
                 {
-                    dc.DrawRectangle(brush, pen, new Rect(x, blockY, barWidth - 2, 3));
+                    dc.DrawRectangle(brush, pen, new Rect(x, blockY, barWidth - 2, 6));
                 }
 
-                // Draw scan lines
-                if (i % 2 == 0)
+                // Draw retro scan lines across entire screen
+                if (i % 4 == 0)
                 {
-                    dc.DrawLine(new Pen(Brushes.Black, 0.5), 
-                        new Point(0, y), new Point(400, y));
+                    dc.DrawLine(new Pen(Brushes.Black, 1), 
+                        new Point(0, y), new Point(screenWidth, y));
                 }
             }
+
+            // Draw retro game border
+            var borderPen = new Pen(Brushes.White, 3);
+            dc.DrawRectangle(null, borderPen, new Rect(10, 10, screenWidth - 20, screenHeight - 20));
         }
     }
 }
